858d956526c6032c15f0ebabb10132d7e4eb737c
Cristobal Mauras <cristobal.mauras@gmail.com>
2025-07-17 19:01:26 -0400

    test menu mob

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.getElementById("navDeploy");
+  const navLink = document.getElementById('navLink');
+  const navList = navbar.querySelector('ul');
+
+  const toggleNavBar = (navbarElement, navLink) => {
+      navLink.classList.toggle("active");
+      navbarElement.classList.toggle("open");
+  };
+
+  navLink.addEventListener('click', () => toggleNavBar(navbar, navLink));
+
+  navList.addEventListener('click', (event) => {
+      if (event.target.tagName === 'A' || event.target.tagName === 'BUTTON') {
+          toggleNavBar(navbar, navLink);
+      }
+  });
+});
